2f455b25d87797276358ed24efc23bd4a6ccf2e9
Disha2426 <23102039@apsit.edu.in>
2026-09-24 12:37:03 +0530

    Update login page title for deployment context

--- pages/login/index.js
+++ pages/login/index.js
@@ -12,7 +12,7 @@ const LoginPage = () => {
     }
   }, [router]);
   return (
-    <Layout title={"Login"}>
+    <Layout title={"Disha's Login - Deployed via Argo CD"}>
       <div className="w-full h-full max-h-[800px] mt-10">
         <Form usedIn={loginAs} setLoginAs />
       </div>
